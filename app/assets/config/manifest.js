a2f8a26a9688cc0820d5c123f495652bb6c3fefc
JoaoVicDias <joovictordias@hotmail.com>
2024-06-14 01:39:41 -0300

    deploy config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
-//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link jquery.min.js
 //= link application.bootstrap.css
+
+//= link_tree ../builds
